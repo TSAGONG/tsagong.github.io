14e52527d0fbacb8226db61fd8d83f9e996ab2f9
Tenzin Tsagong <tsagong@gmail.com>
2017-05-13 11:13:33 -0400

    rome-pic

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ $(".bk").hover(function(){
   });
 
 $(".name").hover(function(){
-    $("#slide2").css('background-image', "url('images/rome.jpg')");
+    $("#slide2").css('background-image', "url('images/Rome.jpg')");
   }, function(){
     $("#slide2").css({"background-color": "white"});
   });
