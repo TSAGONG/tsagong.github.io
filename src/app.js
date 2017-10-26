6b3423ceebc367ad3900e08cdd1050a445c56a8e
Tenzin Tsagong <tsagong@gmail.com>
2017-10-25 23:31:46 -0700

    small changes

--- src/app.js
+++ src/app.js
@@ -1,64 +1,64 @@
 $(document).ready(function() {
 
-$('span').hover(function(){
-    $(this).attr('data-content','bar');
-});
-
-
-/*  $('a[href*=#]').each(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname
-
-    && this.hash.replace(/#/,'') ) {
-      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-       if ($target) {
-         var targetOffset = $target.offset().top;
-
-//On this part of the jQuery code, we’re creating a click event handler to all the links
-//that have “#” symbol anywhere in the href. The very next line, we will check if the link
-//is pointing to the same page by checking for a match between location.pathname. We can
-//then make sure that the link includes a qualified URL or just an identifier.
-
-
-    $(this).click(function() {
-    $("#nav li a").removeClass("active");
-      $(this).addClass('active');
-        $('html, body').animate({scrollTop: targetOffset}, 100);
-          return false;
-    });
-    }
-    }});*/
-
-$(".email").hover(function(){
-    $("#slide4").css({"background-color": "orange"});
-  }, function(){
-    $("#slide4").css({"background-color": "white"});
+  $('span').hover(function() {
+    $(this).attr('data-content', 'bar');
   });
 
-$(".email").hover(function(){
-  $("ul li a").css({"color": "orange"});
-}, function(){
-  $("ul li a").css({"color": "black"});
-});
+
+  /*  $('a[href*=#]').each(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      && location.hostname == this.hostname
+
+      && this.hash.replace(/#/,'') ) {
+        var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+        var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+         if ($target) {
+           var targetOffset = $target.offset().top;
+
+  //On this part of the jQuery code, we’re creating a click event handler to all the links
+  //that have “#” symbol anywhere in the href. The very next line, we will check if the link
+  //is pointing to the same page by checking for a match between location.pathname. We can
+  //then make sure that the link includes a qualified URL or just an identifier.
 
 
-$(".nepal").hover(function(){
+      $(this).click(function() {
+      $("#nav li a").removeClass("active");
+        $(this).addClass('active');
+          $('html, body').animate({scrollTop: targetOffset}, 100);
+            return false;
+      });
+      }
+      }});*/
+
+  $(".email").hover(function() {
+    $("#slide4").css({ "background-color": "orange" });
+  }, function() {
+    $("#slide4").css({ "background-color": "white" });
+  });
+
+  $(".email").hover(function() {
+    $("ul li a").css({ "color": "orange" });
+  }, function() {
+    $("ul li a").css({ "color": "black" });
+  });
+
+
+  $(".nepal").hover(function() {
     $("#slide2").css('background-image', "url('images/nepal.JPG')");
-  }, function(){
-    $("#slide2").css({"background-color": "white"});
+  }, function() {
+    $("#slide2").css({ "background-color": "white" });
   });
 
-$(".bk").hover(function(){
+  $(".bk").hover(function() {
     $("#slide2").css('background-image', "url('images/big.jpg')");
-  }, function(){
-    $("#slide2").css({"background-color": "white"});
+  }, function() {
+    $("#slide2").css({ "background-color": "white" });
   });
 
-$(".name").hover(function(){
+  $(".name").hover(function() {
     $("#slide2").css('background-image', "url('images/Rome.jpg')");
-  }, function(){
-    $("#slide2").css({"background-color": "white"});
+  }, function() {
+    $("#slide2").css({ "background-color": "white" });
   });
 
 });
